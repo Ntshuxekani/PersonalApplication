e2ec5fac5c9c802d51a3214283641ba291806f9a
Ntshuxekani <ntshuxekanizitha@gmail.com>
2024-07-16 16:26:15 +0200

    Photography

---
+++ photographer-app/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './Component/profile/profile.component';
+import { HomeComponent } from './Component/home/home.component';
+import { BookingComponent } from './Component/booking/booking.component';
+import { ReviewsComponent } from './Component/reviews/reviews.component';
+import { LoginComponent } from './Component/login/login.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'portfolio', component: ProfileComponent},
+  { path: 'booking', component: BookingComponent },
+  { path: 'reviews', component: ReviewsComponent },
+  { path: 'login', component: LoginComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ photographer-app/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Component/home/home.component';
+import { ProfileComponent } from './Component/profile/profile.component';
+import { PortifolioComponent } from './Component/portifolio/portifolio.component';
+import { BookingComponent } from './Component/booking/booking.component';
+import { ReviewsComponent } from './Component/reviews/reviews.component';
+import { LoginComponent } from './Component/login/login.component';
+import { SearchComponent } from './Component/search/search.component';
+import { RegisterComponent } from './Component/register/register.component';
+import { NavbarComponent } from './Component/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    PortifolioComponent,
+    BookingComponent,
+    ReviewsComponent,
+    LoginComponent,
+    SearchComponent,
+    RegisterComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
